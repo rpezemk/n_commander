65eb8e31c2ea793f2750e1a94d0c5fe08c88bd4c
rpezemk <rpezemk@gmail.com>
2024-11-11 18:56:29 +0100

    non_ui_tasks to ui_layout (sic!)

--- ncommander.py
+++ ncommander.py
@@ -1,6 +1,7 @@
 import curses
 import signal
 import asyncio
+import time
 from typing import Any
 
 from tui import signal_resolver
@@ -8,20 +9,11 @@ from tui.visual_grid import MainGrid
 from nosc.twin_service import TwinService
 import ui_layout
 from utils.async_processor import AsyncProcessor
-
-async def update_progress_bar():
-    while True:
-        ui_layout.prog_bar_value = ((ui_layout.prog_bar_value * 10 + 10) % 1000)/10
-        await asyncio.sleep(0.05)
         
-async def start_update_progress_bar():
-        asyncio.create_task(update_progress_bar())
-
-
 def main(stdscr):
     try:
         signal_resolver.init_screen(stdscr)
-        asyncio.run(ui_layout.vg.set_stdscr(stdscr).run_async_tasks([start_update_progress_bar]))
+        asyncio.run(ui_layout.vg.set_stdscr(stdscr).run_async_tasks())
     except:
         ...
 
@@ -35,7 +27,13 @@ def wrap():
     signal.signal(signal.SIGTSTP, signal.SIG_IGN)
     twin_service = TwinService("127.0.0.1", 8002, [("/*", server_message_handler)])
     twin_service.push_message(("/test", [99, "Hello OSC!"]))
-    async_proc = AsyncProcessor([twin_service.start_server, twin_service.start_client]).start()
+    async_proc = AsyncProcessor(
+        [
+            twin_service.start_server, 
+            twin_service.start_client, 
+            *ui_layout.non_ui_tasks
+        ])
+    async_proc.start()
     curses.wrapper(main);
     
 if __name__ == "__main__":
