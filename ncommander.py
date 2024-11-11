b1516a07119e4035a566ec04f75c92e892ab529a
rpezemk <rpezemk@gmail.com>
2024-11-11 20:17:34 +0100

    main clean-up

--- ncommander.py
+++ ncommander.py
@@ -1,14 +1,13 @@
 import curses
 import signal
 import asyncio
-import time
 from typing import Any
 
 from tui import signal_resolver
-from tui.visual_grid import MainGrid
 from utils.twin_service import TwinService
 import ui_layout
 from utils.async_processor import AsyncProcessor
+import msg_handlers
         
 def main(stdscr):
     try:
@@ -16,16 +15,10 @@ def main(stdscr):
         asyncio.run(ui_layout.vg.set_stdscr(stdscr).run_async_tasks())
     except:
         ...
-
-def server_message_handler(address, *args):
-    # keep line below for testing purpose
-    # print(f"Received message at {address}: {args}")
-    ...
-
-
+        
 def wrap():
     signal.signal(signal.SIGTSTP, signal.SIG_IGN)
-    twin_service = TwinService("127.0.0.1", 8002, [("/*", server_message_handler)])
+    twin_service = TwinService("127.0.0.1", 8002, msg_handlers.msg_handlers)
     twin_service.push_message(("/test", [99, "Hello OSC!"]))
     async_proc = AsyncProcessor(
         [
